08ab70796fe6d2a0511b8f9a5ca4f2157e8bc874
Edvinas <edvinas.reg@outlook.com>
2016-10-15 11:22:59 +0100

    Some changes

--- AugmentedUno/app/src/main/java/uk/ac/gla/dcs/augmenteduno/TestActivity.java
+++ AugmentedUno/app/src/main/java/uk/ac/gla/dcs/augmenteduno/TestActivity.java
@@ -1,8 +1,9 @@
 package uk.ac.gla.dcs.augmenteduno;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,12 +14,21 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_window);
         Button next = (Button) findViewById(R.id.camera_btn);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), PreviewDemo.class);
-                startActivityForResult(myIntent, 0);
-            }
 
-        });
+        // bluetooth stuff
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        if (next != null) {
+            next.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent myIntent = new Intent(view.getContext(), PreviewDemo.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+            });
+        }
     }
 }
